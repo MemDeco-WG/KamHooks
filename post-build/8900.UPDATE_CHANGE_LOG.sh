6c4f3a6bb43b3cd784350209de76668d15e55b75
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-29 07:46:25 +0800

    Update 8900.UPDATE_CHANGE_LOG.sh

--- post-build/8900.UPDATE_CHANGE_LOG.sh
+++ post-build/8900.UPDATE_CHANGE_LOG.sh
@@ -3,7 +3,7 @@
 # shellcheck source=../lib/utils.sh
 # shellcheck source=Kam/KamHooks/lib/utils.sh
 . "$KAM_HOOKS_ROOT/lib/utils.sh"
-
+is_termux && exit 0
 # optionally update changelog using commitizen.
 require_command cz "commitizen not found; cannot update changelog." || exit 0
 
